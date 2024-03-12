2ec1ee45d3f091b6a60e812d0c8621adeb296ce8
CoderB7 <iutstudent2022@gmail.com>
2024-03-12 07:49:20 +0500

    Fixed Django_settings_module?

--- qtrivia/settings.py
+++ qtrivia/settings.py
@@ -23,7 +23,8 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-xbyy_+3j4==*4naj9)m@ug9+oo^g8sk&kl@egudxe(s6d-5)p="
+# os.environ.get("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
