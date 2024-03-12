f420f67b57e9ae36d0ba23d3e4be7e6665594ffa
CoderB7 <iutstudent2022@gmail.com>
2024-03-12 09:47:58 +0500

    Changed database to postgresql on local machine

--- qtrivia/settings.py
+++ qtrivia/settings.py
@@ -82,16 +82,25 @@ WSGI_APPLICATION = 'qtrivia.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qtrivia_db',
+        'USER': 'qtrivia',
+        'PASSWORD': 'qwerty',
     }
 }
 
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
