8e98b6b7a96a612342953e114d92ad4fad62ff7a
CoderB7 <iutstudent2022@gmail.com>
2024-03-12 09:28:24 +0500

    Changed profile.html and models.py

--- account/models.py
+++ account/models.py
@@ -19,9 +19,9 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.photo.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.photo.path)
+        # img = Image.open(self.photo.path)
+        # if img.height > 300 or img.width > 300:
+        #     output_size = (300, 300)
+        #     img.thumbnail(output_size)
+        #     img.save(self.photo.path)
 
